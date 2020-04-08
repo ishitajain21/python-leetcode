eba43a452b136bc9b5c144dc9a359dec40a7b040
ishitajain21 <40191155+ishitajain21@users.noreply.github.com>
2020-04-07 21:07:29 -0700

    Updated version of the solution to 1313
    Added comments.

--- 1313-Decompress-Run-Length-Encoded-List.py
+++ 1313-Decompress-Run-Length-Encoded-List.py
@@ -1,14 +1,22 @@
-
+# Input: [1,2,3,4] Output: [2,4,4,4]
+# The first element tells the frequency and the next element tells the number. 
 nums = [1,2,3,4]
+
+#The output list. 
 l = []
 
+# Stores the even elements. 
 freq = 0 
 
-
 for i in nums:
+    # Checks if the number is of an odd or even index. 
+    # Even index elements all tell the frequency while Odd index are the elements.
     if nums.index(i)%2 == 1:
+        # Make the frequency of the element. 
         for j in range(0,freq): 
+            # Put that in the output list. 
             l.append(i)
     if nums.index(i)%2 ==0:
-         freq = i
+        # Updatae the frequency of the element to that new one. 
+        freq = i
 print(l)
