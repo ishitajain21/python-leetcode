83351d8ff8fc10fc5f9985b3cccf99061a05b870
ishitajain21 <40191155+ishitajain21@users.noreply.github.com>
2020-04-07 20:25:08 -0700

    In the update, I cleared up my code.

--- 1108-defanging-an-ip-address.py
+++ 1108-defanging-an-ip-address.py
@@ -1,2 +1,6 @@
+# Input: "1.1.1.1". Output: "1[.]1[.]1[.]1[.]" 
+
+# add string is the input. 
 add = "1.1.1.1"
+# This will replace "." with "[.]" creating the output. 
 print(add.replace(".","[.]"))
