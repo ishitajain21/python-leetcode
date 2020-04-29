e9820d1ecdad200a0c994aac4ca7022ba884a481
ishitajain21 <40191155+ishitajain21@users.noreply.github.com>
2020-04-28 20:16:53 -0700

    Added comments that make the code more understandable.

--- 1370_increasing_decreasing_string.py
+++ 1370_increasing_decreasing_string.py
@@ -1,48 +1,49 @@
+# Input: "aaabbbccc" Output: "abccbaabccba"
+# Algorithm:
+    # Pick the smallest character from s and append it to the result.
+    # Pick the smallest character from s which is greater than the last appended character to the result and append it.
+    # Repeat step 2 until you cannot pick more characters.
+    # Pick the largest character from s and append it to the result.
+    # Pick the largest character from s which is smaller than the last appended character to the result and append it.
+    # Repeat step 5 until you cannot pick more characters.
+    # Repeat the steps from 1 to 6 until you pick all characters from s.
+    # In each step, If the smallest or the largest character appears more than once you can choose any occurrence and append it to the result.
 
-# Given a string s. You should re-order the string using the following algorithm:
 
-# Pick the smallest character from s and append it to the result.
-# Pick the smallest character from s which is greater than the last appended character to the result and append it.
-# Repeat step 2 until you cannot pick more characters.
-# Pick the largest character from s and append it to the result.
-# Pick the largest character from s which is smaller than the last appended character to the result and append it.
-# Repeat step 5 until you cannot pick more characters.
-# Repeat the steps from 1 to 6 until you pick all characters from s.
-# In each step, If the smallest or the largest character appears more than once you can choose any occurrence and append it to the result.
-
-# Return the result string after sorting s with this algo 
-
-inp = "aaabbbccc"
-out_for_check = ""
-out = ""
-
-def check_till_everything_is_gone():
-    global inp
-    global out
-    global out_for_check
-    if inp == "":
-        print(out)
+# This function checks that all of the elements in inp are used in the output( This doesn't include the largest character reversal.)
+def check_for_each_element_in_inp():
+    # Checks that the input is empty( for error handling and that inp's elements are removed as they are put in the output.)
+    if inp == "": print(out)
     else:
+        # Calls these functions and then recursively calls itself. 
         pick_char_out()
         repeat_steps()
-        check_till_everything_is_gone()
+        check_for_each_element_in_inp()
 
 def pick_char_out():
+    # Global is used so that the variables can be toggled inside the function. 
     global out
     global out_for_check
+    # The basic essence of the input is in start. No repeated letters. 
     start = ""
-    for i in inp:
+    # The for loop goes though everything in the input and puts the basic essence in the variable start. 
+    for i in inp: 
         if i not in start: start += i
+    # Appends the basic essence in the out_for_check
     out_for_check += start
+    #Since the procedure asks for the letters in both front and back order, the [::-1] reverses it. 
     out += start + start[::-1]
 
 def repeat_steps():
     global inp
-    for i in out_for_check:
-        inp = inp.replace(i,"",1)
-    
-        
-check_till_everything_is_gone()
+    # As mentioned in the check_for function, the occurences in out_for_check are removed from inp. 
+    for i in out_for_check: inp = inp.replace(i,"",1)
 
+# The input:
+inp = "aaabbbccc"
+# This version doesn't have the largest to smallest letter step done so that we can check which letters have been used from inp.
+out_for_check = ""
+#This is the version that is printed out to the user. 
+out = ""
 
-
+check_for_each_element_in_inp()
